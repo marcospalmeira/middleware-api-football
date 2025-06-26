a151399d71fa4d67e6f047c4202304e6e920d443
marcospalmeira <marcos.palmeira@gmail.com>
2025-06-26 14:08:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-app.get('/', (req, res) => {
-  res.send('API Garmin Proxy ativa! Usa /liga-portuguesa/live para dados ao vivo.');
-});
-
 import express from 'express';
 import axios from 'axios';
 import dotenv from 'dotenv';
 import cors from 'cors';
+
+app.get('/', (req, res) => {
+  res.send('API Garmin Proxy ativa! Usa /liga-portuguesa/live para dados ao vivo.');
+});
 
 dotenv.config();
 const app = express();
